1b45cd56b8c2bbafef0f592e13d2b1a0f9cd9e1d
carokann1945 <carokann1945@gmail.com>
2026-03-11 00:39:36 +0900

    style: motion 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { motion } from 'motion/react';
+
+const variants = {
+  enter: {
+    opacity: 0,
+    y: 10,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
   return (
-    <>
+    <motion.div
+      key="result"
+      variants={variants}
+      initial="enter"
+      animate="visible"
+      transition={{ duration: 0.5, ease: 'easeInOut' }}>
       <Header />
       <main>
         <section>
@@ -12,7 +29,7 @@ function App() {
         </section>
       </main>
       <Footer />
-    </>
+    </motion.div>
   );
 }
 
